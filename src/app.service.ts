f025c1db327b990f7a444f15ebe0461584cb0391
68010876-Phoomrapee <68010876@kmitl.ac.th>
2026-03-25 10:15:56 +0700

    00

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello NestJS';
   }
 }
